52969708ad4371bce69e88f05d3601f315a6c4be
Yuliya Zozulya <djulia@ukr.net>
2022-08-11 00:03:23 +0300

    Рендерить шаблон елементів галереї

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListContainer = document.querySelector('.gallery')
+
+function creategalleryItemsMarkUp (items) {
+   
+   const itemsSet = items.reduce((acc, item) => { return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`}, '')
+
+  return itemsSet
+     console.log(itemsSet)
+}
+
+galleryListContainer.innerHTML = creategalleryItemsMarkUp(galleryItems)
+console.log(galleryListContainer)
+
