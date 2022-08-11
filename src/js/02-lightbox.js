4971d04fa1e5141c54b83c2e832fa2b78d8ff318
Yuliya Zozulya <djulia@ukr.net>
2022-08-11 23:22:42 +0300

    Завершує виконання домашнього завдання 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListContainer = document.querySelector('.gallery')
+
+function creategalleryItemsMarkUp (items) {
+   
+   const itemsSet = items.reduce((acc, item) => { 
+    
+    const template = `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`
+    return acc += template}, '')
+    
+    galleryListContainer.innerHTML = itemsSet
+   
+}
+
+creategalleryItemsMarkUp(galleryItems)
+
+let lightbox = new SimpleLightbox('.gallery__link', {captionDelay: 250, captionsData: 'alt'})
